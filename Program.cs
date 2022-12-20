9f078562037e9277d404826e0bf3f04c6d571109
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2022-12-19 21:44:31 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Final_Project
+{
+    class Program
+    {
+        private const int ADD = 1;
+        private const int DELETE = 2;
+        private const int EDIT = 3;
+        private const int LIST = 4;
+        private const int EXIT = 5;
+
+        static int GetMenuChoice()
+        {
+            Console.WriteLine("Student Club Management");
+            Console.WriteLine("1. Add a student");
+            Console.WriteLine("2. Delete a student");
+            Console.WriteLine("3. Edit a student");
+            Console.WriteLine("4. List the students");
+            Console.WriteLine("5. Exit");
+            Console.WriteLine("Select option: ");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            return option;
+        }
+        static void Main(string[] args)
+        {
+            
+            Student Student = new Student();
+            int choice = GetMenuChoice();
+            while (choice != EXIT) 
+            {
+                switch (choice) {
+                    case ADD:
+                        Student.AddStudent();
+                        break;
+                    case DELETE:
+                        Student.DeleteStudent();
+                        break;
+                    case EDIT:
+                        Student.EditStudent();
+                        break;
+                    case LIST:
+                        Student.ListStudents();
+                        break;
+
+                }
+                choice = GetMenuChoice();
+            }
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Final_Project
+{
+    public class Student
+    {
+        private string firstName;
+        private string lastName;
+        private string email;
+        private List<Student> allStudents = new List<Student>();
+        public Student(string first, string last, string mail)
+        {
+
+            firstName = first;
+            lastName = last;
+            email = mail;
+        }
+
+        public Student() : this("Anonymous", "Anonymous", "guest@example.com")
+        {
+
+        }
+        public override string ToString() {
+            return String.Format($"{firstName} {lastName} | {email}");
+            }
+
+        public void AddStudent()
+        {
+            Console.WriteLine("First name: ");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Last name: ");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Email address: ");
+            string email = Console.ReadLine();
+            Student addStudent = new Student(firstName, lastName, email);
+            allStudents.Add(addStudent);
+        }
+        public void DeleteStudent()
+        {
+            if (allStudents.Count == 0)
+            {
+                Console.WriteLine("There is no students in the list.");
+            }
+            for (int i = 0; i < allStudents.Count; i++)
+            {
+                ListStudents();
+                    Console.WriteLine("Enter # to delete");
+                int num = Convert.ToInt32(Console.ReadLine());
+                allStudents.RemoveAt(num - 1);
+                break;
+            }
+            
+        }
+        public void EditStudent()
+        {
+            Console.WriteLine("Enter # to edit ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter new first name: ");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Enter new last name: ");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Enter new email: ");
+            string email = Console.ReadLine();
+            allStudents.RemoveAt(num - 1);
+            Student addStudent = new Student(firstName, lastName, email);
+            allStudents.Insert(num - 1,addStudent);
+        }
+        public void ListStudents()
+        {
+            if (allStudents.Count == 0)
+            {
+                Console.WriteLine("There is no students in the list.");
+                AddStudent();
+            }
+            for (int i=0; i < allStudents.Count; i++)
+            {
+                Student aStudent = allStudents[i];
+                Console.WriteLine($"{i+1}. {aStudent.firstName.ToString()} {aStudent.lastName.ToString()} | {aStudent.email.ToString()}");
+                
+                
+            }
+            
+        }
+
+    }
+}
